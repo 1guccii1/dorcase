a64e2a307a96ee0007237b2255b2ad3a72870312
Kiputar25 <159192353+Kiputar25@users.noreply.github.com>
2025-07-03 22:30:55 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import json
+
+# Load your JSON
+with open("Q&A.json", "r") as f:
+    data = json.load(f)
+
+st.title("My Q&A App 🤖✨")
+
+for q in data:
+    st.subheader(f"Q: {q['question']}")
+    st.write(f"A: {q['answer']}")
